062992556245e9150dd96f405996e00db1ab74ae
Gul Mariam <mariam2@illinois.edu>
2016-03-02 17:55:36 -0600

    Rearranging the news feed page

--- src/application/views/home/index.php
+++ src/application/views/home/index.php
@@ -1,8 +1,12 @@
 <?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>
+<br>
+<br>
+<br>
+<br>
 <p>You have successfully logged in! YAY!</p>
 
-<a href="<?php echo URL_WITH_INDEX_FILE ?>event/create_event">Create Event</a>
+<a href="event/createEvent">Create Event</a>
 <br>
-
+<br>
 <a href="<?php echo URL_WITH_INDEX_FILE ?>user/logout">Logout</a>
 
