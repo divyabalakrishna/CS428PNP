3d35401a89814b6f0ceb209ec616432f140e2057
jhj400 <Hong@wirelessprvnat-172-17-111-165.near.illinois.edu>
2016-03-03 22:03:19 -0600

    update user profile to save into database

--- src/application/views/user/user_profile.php
+++ src/application/views/user/user_profile.php
@@ -1,4 +1,4 @@
-<!--<?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>-->
+<?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>
 
 <div class="container">
 	<!-- Photo -->
@@ -7,19 +7,19 @@
 		<div class="form-group">
 	  		<label for="firstName" class="col-sm-2 control-label">First Name</label>
 	    	<div class="col-sm-10">
-	    		<input type="firstname" class="form-control" id="firstname" value="<?php echo $profileInfo->FirstName ?>" placeholder="First Name">
+	    		<input type="firstname" class="form-control" id="firstname" name="firstname" value="<?php echo $profileInfo->FirstName ?>" placeholder="First Name">
 	    	</div>
 		</div>
 		<div class="form-group">
 			<label for="lastname" class="col-sm-2 control-label">Last Name</label>
 			<div class="col-sm-10">
-				<input type="lastname" class="form-control" id="lastname" value="<?php echo $profileInfo->LastName ?>" placeholder="Last Name">
+				<input type="lastname" class="form-control" id="lastname" name="lastname" value="<?php echo $profileInfo->LastName ?>" placeholder="Last Name">
 	    	</div>
 	    </div>
 	    <div class="form-group">
 			<label for="nickname" class="col-sm-2 control-label">Nick Name</label>
 			<div class="col-sm-10">
-				<input type="nickname" class="form-control" id="nickname" value="" placeholder="Nick Name">
+				<input type="nickname" class="form-control" id="nickname" name="nickname" value="" placeholder="Nick Name">
 	    	</div>
 	    </div>
 	    
@@ -38,21 +38,21 @@
 		<div class="form-group">
 			<label for="gender" class="col-sm-2 control-label">Gender</label>
 			<div class="col-sm-10">
-				<input type="gender" class="form-control" id="gender" placeholder="Gender">
+				<input type="gender" class="form-control" id="gender" name="gender" placeholder="Gender">
 	    	</div>
 	    </div>
 	    
 	    <div class="form-group">
 			<label for="phone" class="col-sm-2 control-label">Phone</label>
 			<div class="col-sm-10">
-				<input type="phone" class="form-control" id="phone" value="<?php echo $profileInfo->Phone ?>" placeholder="Phone">
+				<input type="phone" class="form-control" id="phone" name="phone" value="<?php echo $profileInfo->Phone ?>" placeholder="Phone">
 	    	</div>
 	    </div>
 	    
 	    <div class="form-group">
 			<label for="email" class="col-sm-2 control-label">Email</label>
 			<div class="col-sm-10">
-				<input type="email" class="form-control" id="email" value="<?php echo $profileInfo->Email ?>" placeholder="test@test.com">
+				<input type="email" class="form-control" id="email" name="email" value="<?php echo $profileInfo->Email ?>" placeholder="test@test.com">
 	    	</div>
 	    </div>
 	  
