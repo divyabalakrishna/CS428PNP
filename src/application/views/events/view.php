8d11ab6bc7dd9ef333785e94342bd3f35384d920
jhj400 <Hong@192.168.10.53>
2016-03-14 23:45:34 -0500

    add comments

--- src/application/views/events/view.php
+++ src/application/views/events/view.php
@@ -49,14 +49,69 @@
 				<?php if ($userID == $event->HostID) { ?>
 					<button type="button" id="edit" class="btn btn-default">Edit</button>
 					<button type="button" id="delete" class="btn btn-default">Delete</button>
-				<?php } else { ?>
+				<?php } else if ($userID == $event->HostID) { ?>
 					<!-- TODO: Join/Leave Event -->
+					
+				<?php } else { ?>
+					<button type="button" id="join" class="btn btn-default">Join</button>
 				<?php } ?>
 			</div>
 		</div>
 	</div>
 
 	<!-- TODO: Participant List -->
+	
+	<div class="table-responsive">
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>Comment ID</th>
+					<th>User ID</th>
+					<th>Comment</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach ($comments as $comment) { ?>
+					<tr>
+						<!-- 
+						<td>
+							<a href="<?php echo URL_WITH_INDEX_FILE . "events/view/" . $event->EventID; ?>">
+								<?php echo $event->Name ?>
+							</a>
+						</td>
+						<td class="truncate"><?php echo $event->Description ?></td> -->
+						<td><?php echo $comment->CommentID ?></td>
+						<td><?php echo $comment->UserID ?></td>
+						<td><?php echo $comment->Text ?></td>
+					</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+	</div>
+	
+	<form id="form" method="post" action="<?php echo URL_WITH_INDEX_FILE; ?>events/reply" enctype="multipart/form-data" class="form-horizontal">
+		<input type="hidden" id="eventID" name="eventID" value="<?php echo $event->EventID ?>" />
+		<input type="hidden" id="userID" name="userID" value="<?php echo $userID ?>" />
+		<input type="hidden" id="parentID" name="parentID" value="<?php echo $event->EventID ?>" />
+
+		<!-- Name -->
+		<div class="form-group">
+			<label for="name" class="col-sm-2 control-label">Comment</label>
+			<div class="col-sm-10">
+				<input type="textview" id="text" name="text" class="form-control" required aria-required="true">
+			</div>
+		</div>
+		
+		<!-- Buttons -->
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<button type="submit" class="btn btn-default">Save</button>
+				<!-- 
+				<button type="button" id="cancel" class="btn btn-default">Cancel</button>  -->
+			</div>
+		</div>
+	</form>
+</div>
 </div>
 
 <script>
