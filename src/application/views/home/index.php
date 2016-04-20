f10304296e874c967a1776592880cf8caf023628
divya <balakrishnadivya@gmail.com>
2016-04-20 13:26:07 -0500

    display tags on menu
    Change-Id: I6731d9c430413e719a17e21b831997916f7c46e1

--- src/application/views/home/index.php
+++ src/application/views/home/index.php
@@ -1,13 +1,14 @@
 <?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>
 <div class="splitter">
 	<?php echo $GLOBALS["beans"]->siteHelper->getAlertsHTML(); ?>
-	<?php $Events = $hostedEvents ?>
+	<?php $Events = $joinedEvents ?>
     <div class="leftpaneCollapsed" id="leftpane">
 	    <button class="buttons" id="hosted">Hosted</button>
 	    <button class="buttons" id="past">Past</button>
 	    <button class="buttons" id="joined">Joined</button>
 	    <button class="buttons" id="feed">Feed</button>
 	    <button class="buttons" id="create">create</button>
+	    <div id="tagsList" class="tagList"></div>
     </div>
     <div class="rightpane rightpaneExpanded" id="rightpane">
     	<?php foreach ($Events as $event) { ?>
@@ -20,8 +21,7 @@
 					<div class="title"><?php echo $event->Name ?></div>
 					<div class="desc"><?php echo $event->Description ?></div>
 					<div class="desc"><?php echo $event->Address ?></div>
-					<div class="desc"><?php echo $event->FormattedDate ?></div>
-					<div class="desc"><?php echo $event->FormattedTime ?></div>
+					<div class="desc"><?php echo $event->FormattedDate ?> at <?php echo $event->FormattedTime ?></div>
 				</div>
 		    </div>
 	    <?php } ?>
@@ -33,8 +33,28 @@
 		var eventID = $(this).children()[0].value;
 		self.location='<?php echo URL_WITH_INDEX_FILE; ?>events/view/' + eventID;
 	}
+	function loadTags(){
+		<?php $Events = $joinableEvents ?>
+		var tagsList = [];
+		$('#tagsList').empty();
+		<?php foreach ($Events as $event) { ?>
+			var tag = "<?php echo $event->TagName ?>";
+			var present = 0;
+			for(i = 0; i < tagsList.length; i++){
+				if(tag === tagsList[i]){
+					present = 1;
+				}
+			}
+			if(!present){
+				tagsList[tagsList.length] = "<?php echo $event->TagName ?>";
+				var tag = '<a class="tag">#<?php echo $event->TagName ?></a><br/>';
+				$('#tagsList').append(tag);
+			}
+		<?php } ?>
+	}
     $(function() {
     	$(document).ready(function(){
+        	loadTags();
     		$('#menuHome').click(function(){
 				if(this.toggle){
             		this.toggle = 0;
@@ -54,11 +74,10 @@
     			self.location='<?php echo URL_WITH_INDEX_FILE; ?>events/edit';
         	});
     		$('#feed').click(function(){
-    			
-    		});
-    		$('#joined').click(function(){
-    			<?php $Events = $joinedEvents ?>
+    			<?php $Events = $joinableEvents ?>
     			$('#rightpane').empty();
+    			$('#tagsList').empty();
+    			var tagsList = [];
     			<?php foreach ($Events as $event) { ?>
 				   var tile =  '<div class = "tiles">' +
 				   		'<input type="hidden" id="eventIDTile" name="eventID" value="<?php echo $event->EventID ?>" />'+
@@ -76,12 +95,61 @@
 				    
 				    $('#rightpane').append(tile);
 				    $('.tiles').click(tileClick);
+				    var tag = "<?php echo $event->TagName ?>";
+					var present = 0;
+					for(i = 0; i < tagsList.length; i++){
+						if(tag === tagsList[i]){
+							present = 1;
+						}
+					}
+					if(!present){
+						tagsList[tagsList.length] = "<?php echo $event->TagName ?>";
+						var tag = '<a class="tag">#<?php echo $event->TagName ?></a><br/>';
+						$('#tagsList').append(tag);
+					}
+			    <?php } ?>
+    		});
+    		$('#joined').click(function(){
+    			<?php $Events = $joinedEvents ?>
+    			$('#rightpane').empty();
+    			$('#tagsList').empty();
+    			var tagsList = [];
+    			<?php foreach ($Events as $event) { ?>
+				   var tile =  '<div class = "tiles">' +
+				   		'<input type="hidden" id="eventIDTile" name="eventID" value="<?php echo $event->EventID ?>" />'+
+					    '<div class="icon">'+
+					    	'<img style="width: 100%;height: 100%;" src="<?php echo URL; ?>public/img/sports/<?php echo $event->TagName ?>.png">'+
+						'</div>'+
+						'<div class="content">'+
+							'<div class="title"><?php echo $event->Name ?></div>'+
+							'<div class="desc"><?php echo $event->Description ?></div>'+
+							'<div class="desc"><?php echo $event->Address ?></div>'+
+							'<div class="desc"><?php echo $event->FormattedDate ?></div>'+
+							'<div class="desc"><?php echo $event->FormattedTime ?></div>'+
+						'</div>'+
+				    '</div>';
+				    
+				    $('#rightpane').append(tile);
+				    $('.tiles').click(tileClick);
+				    var tag = "<?php echo $event->TagName ?>";
+					var present = 0;
+					for(i = 0; i < tagsList.length; i++){
+						if(tag === tagsList[i]){
+							present = 1;
+						}
+					}
+					if(!present){
+						tagsList[tagsList.length] = "<?php echo $event->TagName ?>";
+						var tag = '<a class="tag">#<?php echo $event->TagName ?></a><br/>';
+						$('#tagsList').append(tag);
+					}
 			    <?php } ?>
     		});
     		$('#past').click(function(){
     			<?php $Events = $pastEvents ?>
     			$('#rightpane').empty();
-    			debugger;
+    			$('#tagsList').empty();
+    			var tagsList = [];
     			<?php foreach ($Events as $event) { ?>
 				   var tile =  '<div class = "tiles">' +
 				   		'<input type="hidden" id="eventIDTile" name="eventID" value="<?php echo $event->EventID ?>" />'+
@@ -99,11 +167,25 @@
 				    
 				    $('#rightpane').append(tile);
 				    $('.tiles').click(tileClick);
+				    var tag = "<?php echo $event->TagName ?>";
+					var present = 0;
+					for(i = 0; i < tagsList.length; i++){
+						if(tag === tagsList[i]){
+							present = 1;
+						}
+					}
+					if(!present){
+						tagsList[tagsList.length] = "<?php echo $event->TagName ?>";
+						var tag = '<a class="tag">#<?php echo $event->TagName ?></a><br/>';
+						$('#tagsList').append(tag);
+					}
 			    <?php } ?>
     		});
     		$('#hosted').click(function(){
     			<?php $Events = $hostedEvents ?>
     			$('#rightpane').empty();
+    			$('#tagsList').empty();
+    			var tagsList = [];
     			<?php foreach ($Events as $event) { ?>
 				   var tile =  '<div class = "tiles">' +
 				   		'<input type="hidden" id="eventIDTile" name="eventID" value="<?php echo $event->EventID ?>" />'+
@@ -121,6 +203,18 @@
 				    
 				    $('#rightpane').append(tile);
 				    $('.tiles').click(tileClick);
+				    var tag = "<?php echo $event->TagName ?>";
+					var present = 0;
+					for(i = 0; i < tagsList.length; i++){
+						if(tag === tagsList[i]){
+							present = 1;
+						}
+					}
+					if(!present){
+						tagsList[tagsList.length] = "<?php echo $event->TagName ?>";
+						var tag = '<a class="tag">#<?php echo $event->TagName ?></a><br/>';
+						$('#tagsList').append(tag);
+					}
 			    <?php } ?>
     		});
         });
