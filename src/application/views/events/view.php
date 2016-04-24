a4680a298708a3658535c365835ee0b0a688d971
psjoshi2 <paarth_joshi@hotmail.com>
2016-04-24 06:23:38 -0500

    test event page alignment
    test alignment

--- src/application/views/events/view.php
+++ src/application/views/events/view.php
@@ -22,27 +22,10 @@ else if ($event->Capacity > count($participants)) {
 		<div class="form-horizontal eventDetails">
 			<div class="form-group">
 				<label class="col-sm-2 control-label">Description</label>
-				<div class="col-sm-10">
+				<div class="col-sm-3">
 					<p class="form-control-static"><?php echo $event->Description ?></p>
 				</div>
 			</div>
-			<div class="participants">
-			<h3 class="page-header">Participants</h3>
-			<div class="col-sm-6">
-				<?php foreach ($participants as $person) { ?>
-					<div class="profile">
-						<a href="<?php echo URL_WITH_INDEX_FILE . "user/viewParticipantProfile/" . $person->UserID; ?>">
-							<?php if ($person->Picture == "") { ?>
-								<img class="image" src="<?php echo URL; ?>public/img/profile.png">
-							<?php } else { ?>
-								<img class="image" src="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('profile', $person->Picture) ?>"/>
-							<?php } ?>
-								<div class="name"><?php echo $person->FirstName ?></div>
-						</a>
-					</div>
-				<?php } ?>
-			</div>
-			<br><br>
 			<div class="form-group">
 				<label class="col-sm-2 control-label">Date/Time</label>
 				<div class="col-sm-10">
