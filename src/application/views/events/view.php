498bb992687cb095c2516214b4f47082ff6268bf
Felicia Chandra <fchandr2@illinois.edu>
2016-04-24 12:48:26 -0500

    Fixed participant div.

--- src/application/views/events/view.php
+++ src/application/views/events/view.php
@@ -18,9 +18,9 @@ else if ($event->Capacity > count($participants)) {
 		<?php } ?>
 		<div class="title"><?php echo $event->Name ?></div>
 	</div>
-	
-	<div class="col-sm-6">
-		<div class="form-horizontal eventDetails">
+
+	<div class="form-horizontal eventDetails">
+		<div class="col-sm-8">
 			<div class="form-group">
 				<label class="col-sm-1 control-label">Description</label>
 				<div class="col-sm-10">
@@ -76,216 +76,215 @@ else if ($event->Capacity > count($participants)) {
 				</div>
 			</div>
 		</div>
+
+		<div class="col-sm-4">
+			<div class="participants">
+				<h3 class="page-header">Participants</h3>
+				<div class="form-group profilePhotos">
+					<?php foreach ($participants as $person) { ?>
+						<div class="profile">
+							<a href="<?php echo URL_WITH_INDEX_FILE . "user/viewParticipantProfile/" . $person->UserID; ?>">
+								<?php if ($person->Picture == "") { ?>
+									<img class="image" src="<?php echo URL; ?>public/img/profile.png">
+								<?php } else { ?>
+									<img class="image" src="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('profile', $person->Picture) ?>"/>
+								<?php } ?>
+								<div class="name"><?php echo $person->FirstName ?></div>
+							</a>
+						</div>
+					<?php } ?>
+				</div>
+			</div>
+		</div>
 	</div>
-	
-	<div class="form-horizontal attendees">	
-		<div class="participants">
-			<h3 class="page-header">Participants</h3>
-			<div class="form-group profilePhotos">
-				<?php foreach ($participants as $person) { ?>
-					<div class="profile">
-						<a href="<?php echo URL_WITH_INDEX_FILE . "user/viewParticipantProfile/" . $person->UserID; ?>">
-							<?php if ($person->Picture == "") { ?>
-								<img class="image" src="<?php echo URL; ?>public/img/profile.png">
-							<?php } else { ?>
-								<img class="image" src="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('profile', $person->Picture) ?>"/>
-							<?php } ?>
-							<div class="name"><?php echo $person->FirstName ?></div>
-						</a>
-					</div>
-				<?php } ?>
-			</div>
-		</div>
-	</div>	
-	<br><br>
 
-		<!-- Media -->
-		<div class="media">
-			<h3 class="page-header">Media</h3>
-			<?php echo $GLOBALS["beans"]->siteHelper->getAlertsHTML(); ?>
-			<div class="row well">
-				<div class="col-md-12">
-					<div class="carousel carousel-showmanymoveone slide" id="carousel123">
-						<div class="carousel-inner">
-							<?php $cnt = 0;
-							$act = "active";
-							foreach ($media as $image) { 
-								if ($image->Image != "") { 
-									$ext = explode(".", strtolower($image->Image));
-									if ($ext[1] == "mp4") { ?>
-										<div class="item <?php echo "$act"; ?> ">
-											<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-												<a class="image-frame-car" href="" data-toggle="modal" data-target="#showModal" data-id="<?php echo $image->MediaID;?>" data-owner="<?php echo $image->UserID;?>" data-whatever="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('media', $image->Image) ?>" data-type="video">
-												<video class="image-thumb-car">
-													<source src="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('media', $image->Image) ?>" type="video/mp4">
-													<source src="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('media', $image->Image) ?>" type="video/ogg">
-													Your browser does not support HTML5 video.
-												</video>
-												</a>
-											</div>
+	<!-- Media -->
+	<div class="media">
+		<h3 class="page-header">Media</h3>
+		<?php echo $GLOBALS["beans"]->siteHelper->getAlertsHTML(); ?>
+		<div class="row well">
+			<div class="col-md-12">
+				<div class="carousel carousel-showmanymoveone slide" id="carousel123">
+					<div class="carousel-inner">
+						<?php $cnt = 0;
+						$act = "active";
+						foreach ($media as $image) { 
+							if ($image->Image != "") { 
+								$ext = explode(".", strtolower($image->Image));
+								if ($ext[1] == "mp4") { ?>
+									<div class="item <?php echo "$act"; ?> ">
+										<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+											<a class="image-frame-car" href="" data-toggle="modal" data-target="#showModal" data-id="<?php echo $image->MediaID;?>" data-owner="<?php echo $image->UserID;?>" data-whatever="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('media', $image->Image) ?>" data-type="video">
+											<video class="image-thumb-car">
+												<source src="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('media', $image->Image) ?>" type="video/mp4">
+												<source src="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('media', $image->Image) ?>" type="video/ogg">
+												Your browser does not support HTML5 video.
+											</video>
+											</a>
 										</div>
-									<?php } else { ?>
-										<div class="item <?php echo "$act"; ?> ">
-											<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-												<a class="image-frame-car" href="" data-toggle="modal" data-target="#showModal" data-id="<?php echo $image->MediaID;?>" data-owner="<?php echo $image->UserID;?>" data-whatever="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('media', $image->Image) ?>" data-type="image">
-													<div class="image-thumb-car" style="background-image: url('<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('media', $image->Image) ?>');">
-													</div>
-												</a>
-											</div>
+									</div>
+								<?php } else { ?>
+									<div class="item <?php echo "$act"; ?> ">
+										<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+											<a class="image-frame-car" href="" data-toggle="modal" data-target="#showModal" data-id="<?php echo $image->MediaID;?>" data-owner="<?php echo $image->UserID;?>" data-whatever="<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('media', $image->Image) ?>" data-type="image">
+												<div class="image-thumb-car" style="background-image: url('<?php echo $GLOBALS["beans"]->fileHelper->getUploadedFileURL('media', $image->Image) ?>');">
+												</div>
+											</a>
 										</div>
-									<?php }
-								}
-								if ($cnt == "active" && count($media) >= 6) {
-									$act = "";
-								}
-							} ?>
-						</div>
-						<?php if (count($media) >= 6) { ?>
-							<a class="left carousel-control" href="#carousel123" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-							<a class="right carousel-control" href="#carousel123" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
-						<?php } ?>
+									</div>
+								<?php }
+							}
+							if ($cnt == "active" && count($media) >= 6) {
+								$act = "";
+							}
+						} ?>
 					</div>
+					<?php if (count($media) >= 6) { ?>
+						<a class="left carousel-control" href="#carousel123" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+						<a class="right carousel-control" href="#carousel123" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+					<?php } ?>
 				</div>
 			</div>
-			<br>
-			<div class="row text-center">
-				<?php $showUpload = false;
-				foreach ($participants as $participant) {
-					if ($participant->UserID == $userID) {
-						$showUpload = true;
-						break;
-					}
+		</div>
+		<br>
+		<div class="row text-center">
+			<?php $showUpload = false;
+			foreach ($participants as $participant) {
+				if ($participant->UserID == $userID) {
+					$showUpload = true;
+					break;
 				}
-				if ($showUpload) { ?>
-					<button type="submit" class="btn btn-default" data-toggle="modal" data-target="#uploadModal">Upload New Media</button>
-				<?php } ?>
-			</div>
+			}
+			if ($showUpload) { ?>
+				<button type="submit" class="btn btn-default" data-toggle="modal" data-target="#uploadModal">Upload New Media</button>
+			<?php } ?>
 		</div>
+	</div>
 
-		<!-- Modal Upload Media -->
-		<div class="modal fade" id="uploadModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<form class="form-group" id="formUpload" method="post" action="<?php echo URL_WITH_INDEX_FILE; ?>events/upload" enctype="multipart/form-data" >
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-							<h3 class="modal-title" id="myModalLabel"><img class="icon" src="<?php echo URL; ?>public/img/icon.png"> Upload Media</h3>
-						</div>
-	
-						<div class="modal-body">
-							<br>
-							<input type="hidden" id="eventID" name="eventID" value="<?php echo $event->EventID ?>" />
-							<label for="image" class="sr-only">Image</label>
-							<input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
-							<input type="file" id="image" name="image[]" accept="image/jpg,image/jpeg,image/png,image/bmp,video/mp4" multiple class="form-control" />
-							<p class="help-block">Max file size: 2 MB. Accepted file types: .jpg, .jpeg, .png, .bmp</p>
-							<br>
-						</div>
-	
-						<div class="modal-footer">
-							<!-- Buttons -->
-							<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-							<button type="submit" class="btn btn-primary">Upload</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-
-		<!-- Modal Show Media -->
-		<div class="modal fade" id="showModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-			<div class="modal-dialog modal-lg" role="document">
-				<div class="modal-content">
+	<!-- Modal Upload Media -->
+	<div class="modal fade" id="uploadModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<form class="form-group" id="formUpload" method="post" action="<?php echo URL_WITH_INDEX_FILE; ?>events/upload" enctype="multipart/form-data" >
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h3 class="modal-title" id="myModalLabel"><img class="icon" src="<?php echo URL; ?>public/img/icon.png"> Upload Media</h3>
 					</div>
-					<div class="modal-body text-center">
-						<img id="showImage" name="showImage" class="showImage" src="">
-						<video width="100%" id="showVideo" autoplay style="display:none" controls>
-							<source src="" type="video/mp4"></source>
-							Your browser does not support HTML5 video.
-						</video>
+
+					<div class="modal-body">
+						<br>
+						<input type="hidden" id="eventID" name="eventID" value="<?php echo $event->EventID ?>" />
+						<label for="image" class="sr-only">Image</label>
+						<input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
+						<input type="file" id="image" name="image[]" accept="image/jpg,image/jpeg,image/png,image/bmp,video/mp4" multiple class="form-control" />
+						<p class="help-block">Max file size: 2 MB. Accepted file types: .jpg, .jpeg, .png, .bmp</p>
+						<br>
 					</div>
+
 					<div class="modal-footer">
-						<div class="right">
-							<button id="deleteMedia" style="display:none; right;" type="button" class="btn btn-primary" onclick="">Delete</button>
-						</div>
+						<!-- Buttons -->
+						<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+						<button type="submit" class="btn btn-primary">Upload</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<!-- Modal Show Media -->
+	<div class="modal fade" id="showModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				</div>
+				<div class="modal-body text-center">
+					<img id="showImage" name="showImage" class="showImage" src="">
+					<video width="100%" id="showVideo" autoplay style="display:none" controls>
+						<source src="" type="video/mp4"></source>
+						Your browser does not support HTML5 video.
+					</video>
+				</div>
+				<div class="modal-footer">
+					<div class="right">
+						<button id="deleteMedia" style="display:none; right;" type="button" class="btn btn-primary" onclick="">Delete</button>
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 
-		<!-- Comments -->	
-		<div class="comments">
-			<h3 class="page-header">Comments</h3>
-			<table class="table table-striped">
-				<tbody>
-					<?php $parentID = "";
-					foreach ($comments as $comment) { 
-						if ($parentID != "" && $parentID != $comment->ParentID) { ?>
-							<tr>
-								<td>
-									<a onclick="reply(this, <?php echo $parentID ?>)" style="cursor:pointer">reply</a>
-								</td>
-								<td colspan="3"></td>
-							</tr>
-						<?php }
-						if ($comment->ParentID == $comment->CommentID) { ?>
-							<tr>
-								<td class="col-md-1"><?php echo $comment->FirstName ?></td>
-								<td colspan="2"><?php echo $comment->Text ?></td>
-								<td width="1%">
-									<?php if ($comment->UserID == $userID) {?>
-										<span class="glyphicon glyphicon-remove" aria-hidden="true" style="cursor:pointer" onclick="deleteComment(<?php echo $comment->CommentID ?>)"></span>
-									<?php } ?>
-								</td>
-							</tr>
-						<?php } else { ?>
-							<tr>
-								<td></td>	
-								<td class="col-md-1"><?php echo $comment->FirstName ?></td>
-								<td><?php echo $comment->Text ?></td>
-								<td width="1%">
-									<?php if ($comment->UserID == $userID) {?>
-										<span class="glyphicon glyphicon-remove" aria-hidden="true" style="cursor:pointer" onclick="deleteComment(<?php echo $comment->CommentID ?>)"></span>
-									<?php } ?>
-								</td>
-							</tr>
-						<?php }
-						$parentID = $comment->ParentID;
-					}
-					if ($parentID != "") { ?>
+	<!-- Comments -->	
+	<div class="comments">
+		<h3 class="page-header">Comments</h3>
+		<table class="table table-striped">
+			<tbody>
+				<?php $parentID = "";
+				foreach ($comments as $comment) { 
+					if ($parentID != "" && $parentID != $comment->ParentID) { ?>
 						<tr>
 							<td>
 								<a onclick="reply(this, <?php echo $parentID ?>)" style="cursor:pointer">reply</a>
 							</td>
 							<td colspan="3"></td>
 						</tr>
-					<?php } ?>
-				</tbody>
-			</table>
-			
-			<form id="form" method="post" action="<?php echo URL_WITH_INDEX_FILE; ?>events/reply" enctype="multipart/form-data" class="form-horizontal">
-				<input type="hidden" id="eventID" name="eventID" value="<?php echo $event->EventID ?>" />
-				<input type="hidden" id="userID" name="userID" value="<?php echo $userID ?>" />
-				<input type="hidden" id="parentID" name="parentID" value="" />
-		
-				<!-- Name -->
-				<div class="form-group">
-					<div class="col-sm-10">
-						<textarea class="form-control" id="text" name="text" required aria-required="true"></textarea>
-					</div>
+					<?php }
+					if ($comment->ParentID == $comment->CommentID) { ?>
+						<tr>
+							<td class="col-md-1"><?php echo $comment->FirstName ?></td>
+							<td colspan="2"><?php echo $comment->Text ?></td>
+							<td width="1%">
+								<?php if ($comment->UserID == $userID) {?>
+									<span class="glyphicon glyphicon-remove" aria-hidden="true" style="cursor:pointer" onclick="deleteComment(<?php echo $comment->CommentID ?>)"></span>
+								<?php } ?>
+							</td>
+						</tr>
+					<?php } else { ?>
+						<tr>
+							<td></td>	
+							<td class="col-md-1"><?php echo $comment->FirstName ?></td>
+							<td><?php echo $comment->Text ?></td>
+							<td width="1%">
+								<?php if ($comment->UserID == $userID) {?>
+									<span class="glyphicon glyphicon-remove" aria-hidden="true" style="cursor:pointer" onclick="deleteComment(<?php echo $comment->CommentID ?>)"></span>
+								<?php } ?>
+							</td>
+						</tr>
+					<?php }
+					$parentID = $comment->ParentID;
+				}
+				if ($parentID != "") { ?>
+					<tr>
+						<td>
+							<a onclick="reply(this, <?php echo $parentID ?>)" style="cursor:pointer">reply</a>
+						</td>
+						<td colspan="3"></td>
+					</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+
+		<form id="form" method="post" action="<?php echo URL_WITH_INDEX_FILE; ?>events/reply" enctype="multipart/form-data" class="form-horizontal">
+			<input type="hidden" id="eventID" name="eventID" value="<?php echo $event->EventID ?>" />
+			<input type="hidden" id="userID" name="userID" value="<?php echo $userID ?>" />
+			<input type="hidden" id="parentID" name="parentID" value="" />
+
+			<!-- Name -->
+			<div class="form-group">
+				<div class="col-sm-10">
+					<textarea class="form-control" id="text" name="text" required aria-required="true"></textarea>
 				</div>
-				
-				<!-- Buttons -->
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
-						<button type="submit" class="btn btn-default">Save</button>
-					</div>
+			</div>
+
+			<!-- Buttons -->
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" class="btn btn-default">Save</button>
 				</div>
-			</form>
-		</div>
-	
+			</div>
+		</form>
+	</div>
+
 </div>
 
 
